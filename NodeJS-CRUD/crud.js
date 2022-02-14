7a2ba506750bf94b1dd89a1a90f2c1c5b5bbfc29
Koray Tuncer <78158735+koraytuncer@users.noreply.github.com>
2022-02-14 17:24:06 +0300

    Add files via upload

---
+++ NodeJS-CRUD/crud.js
@@ -0,0 +1,73 @@
+const fs = require('fs');
+
+//Create
+const writeFile = (file, data) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(file, data, 'utf8', (err) => {
+      if (err) reject(err);
+      resolve(`${file} "Baþarýlý Bir Þekilde Oluþturuldu"`);
+    });
+  });
+};
+
+//Read
+const readFile = (file) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, 'utf8', (err, data) => {
+      if (err) reject(err);
+      resolve(data);
+    });
+  });
+};
+
+//Update
+const appendFile = (file, data) => {
+  return new Promise((resolve, reject) => {
+    fs.appendFile(file, data, 'utf8', (err) => {
+      if (err) reject(err);
+      resolve(`${file} Baþarýlý Bir Þekilde Eklendi`);
+    });
+  });
+};
+//Delete
+const deleteFile = (file) => {
+  return new Promise((resolve, reject) => {
+    fs.unlink(file, function (err) {
+      if (err) reject(err);
+      resolve('Dosya silindi!');
+    });
+  });
+};
+
+const asyncPro = async () => {
+  try {
+    const file = 'employees.json';
+    const data = '{"name": "Employee 1 Name", "salary": 2000}';
+    let fileWrite = await writeFile(file, data)
+      .then((result) => console.log(result))
+      .catch((err) => console.log(err));
+
+    let fileRead = await readFile(file)
+      .then((data) => {
+        console.log(`${file} Baþarýlý Bir Þekilde Okundu`);
+        console.log(data);
+      })
+      .catch((err) => console.log(err));
+
+    const newData = '{"name": "Employee 2 JJ", "salary": 15000}';
+
+    let fileUpdate = await appendFile(file, '\n' + newData)
+      .then((data) => {
+        console.log(`${file}'e Baþarýlý Bir Þekilde Güncellendi`);
+        console.log(newData);
+      })
+      .catch((err) => console.log(err));
+
+    let fileDelete = await deleteFile(file)
+      .then((result) => console.log(result))
+      .catch((err) => console.log(err));
+  } catch {
+    console.log('Ýþlem Sýrasýnda Bir Hata Oluþtu');
+  }
+};
+asyncPro();
